416c3e1556d4a0ecf4fabf72a6881ab63a3219a3
Kitty <77138477+kamork@users.noreply.github.com>
2023-06-25 00:18:47 -0700

    Create and implement Message model

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Kitty Mork on 2023-06-24.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
