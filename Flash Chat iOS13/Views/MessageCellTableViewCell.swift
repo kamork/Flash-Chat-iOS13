34e0bf1777d1906b24f7062b62fbc2b2e2eae857
Kitty <77138477+kamork@users.noreply.github.com>
2023-06-25 15:15:33 -0700

    Redesign message bubble

---
+++ Flash Chat iOS13/Views/MessageCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageCellTableViewCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Kitty Mork on 2023-06-25.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
